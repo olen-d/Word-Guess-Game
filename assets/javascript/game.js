a7194bd24ca1604c6701de7472bb6fc5b5b4e373
Salvador Dieffenbach <zogwarg@hotmail.com>
2018-12-03 00:52:02 -0500

    Basic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,13 @@
-let wordlist = [
+// Set up all the variables
+
+let wins = 0;
+let guessLimit = 6;
+let anyKeyPressed = false;
+let blanks = "";
+let pos = 0;
+let word = "";
+
+let wordList = [
     "analog",
     "artisanal",
     "beards",
@@ -16,7 +25,74 @@ let wordlist = [
     "organic",
     "Pabst",
     "Polaroid",
-    "pourover"
+    "pourover",
     "vegan",
     "vinyl"
 ]; 
+
+let lettersGuessed = [];
+
+function updateGameStats (statistic, value) {
+    let elementToUpdate = document.getElementById(statistic);
+    elementToUpdate.innerHTML = value;
+}
+
+function replaceBlank (blankPosition, letter) {
+    let blankToUpdate = document.getElementById(blankPosition);
+    blankToUpdate.textContent = letter;
+}
+
+// Start the game when the user presses any key
+
+document.onkeyup = function(event) {
+    let userInput = event.key;
+    if (anyKeyPressed) {
+
+        // Todo: Check to see if a letter is entered using regex
+        // Check to see if the letter has already been guessed
+        let letterIndex = lettersGuessed.indexOf(userInput);
+        if(letterIndex === -1) {
+            // Add the letter to the list of letters guessed and update the letters guessed output
+            lettersGuessed.push(userInput);
+            updateGameStats("already-guessed",lettersGuessed);
+
+            // Check to see if the letter is correct
+            let results = [];
+            let indexes = [];
+            let re = new RegExp(userInput,"gi");
+
+            while ((results = re.exec(word)) !== null) {
+               indexes.push(re.lastIndex);
+            }
+            
+            if (indexes.length > 0) {
+                indexes.forEach(function(element) {
+                    replaceBlank("blank" + element,userInput + " ");
+                });
+
+            } else {
+                guessLimit--;
+                updateGameStats("guesses-remaining",guessLimit);
+                //if guesses = 0, game over
+            }
+            
+            
+        // Check to see if the word is complete
+        }
+        
+    } else {
+        anyKeyPressed = true;
+        // Pick a word
+        let wordIndex = Math.floor(Math.random() * wordList.length);
+        word = wordList[wordIndex];alert(word);
+        let wordLength = word.length;
+
+        // Make the blanks
+        for (i = 1; i <= wordLength; i++)
+            {
+            blanks += "<span id=\"blank" + i + "\">_ </span>";
+            }
+        updateGameStats("blanks",blanks);
+        updateGameStats("guesses-remaining",guessLimit);
+    }
+}
